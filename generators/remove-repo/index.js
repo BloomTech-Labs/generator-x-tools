44eb3abed10abde1bcf29d287047acbcedea6673
Jake Mallory <4095+tinomen@users.noreply.github.com>
2021-03-03 02:37:02 -0700

    add remove-repo

---
+++ generators/remove-repo/index.js
@@ -0,0 +1,58 @@
+const { BaseGenerator, klr, octokit, _inspect } = require("../gh-base");
+
+module.exports = class extends BaseGenerator {
+  constructor(args, opts) {
+    super(args, opts);
+    this.initialData = {};
+    this.githubOwner = "Lambda-School-Labs";
+
+    this._makePromptOption(
+      'repo',
+      {
+        type: 'input',
+        message: 'What is the name of the repo?',
+        store: true,
+      },
+      {
+        type: String,
+        alias: 'p',
+        desc: 'name of the repo',
+      }
+    );
+  }
+
+  initializing() {
+    this.log(
+      `Welcome to the ${klr.red('Labs')} ${klr.bold(
+        'Repo Remover'
+      )}!\nLets get started.`
+    );
+    this._removePrompts();
+    this.initialData = Object.assign({}, this.initialData, this.options);
+  }
+
+  prompting() {
+    return this.prompt(this.prompts).then((props) => {
+      this.answers = props;
+      this.data = Object.assign({}, this.initialData, this.answers);
+    });
+  }
+
+  configuring() {
+    this.log(`Configuring for repo ${klr.bold(
+      this.data.repo
+    )}`);
+    // _inspect(this.data);
+  }
+
+  writing() {
+    (async () => {
+      const repo = await octokit.repos.delete({
+        owner: this.githubOwner,
+        repo: this.data.repo,
+      });
+      // _inspect(repo);
+      this.log(`================\nRemoved repo ${repo.url}.\n`);
+    })();
+  }
+};
